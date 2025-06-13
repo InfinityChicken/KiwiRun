c18319d51595acf6214f15cd0ddc30eddc736461
Infinity Chicken <8zsc2014@gmail.com>
2025-06-12 18:37:02 -0700

    json debug, theta error recode

--- src/kiwirun/ramsete.cpp
+++ src/kiwirun/ramsete.cpp
@@ -2,7 +2,7 @@
 #include "kiwirun/includes.hpp"
 #include "globals.hpp"
 
-std::string debugLine;
+nlohmann::json debugData;
 
 namespace kiwi {
 
@@ -52,7 +52,7 @@ int Path::findClosestPoint(lemlib::Pose pose, int prevIndex) {
     return -1; // you're screwed OR end of path reached
 }
 
-void Path::updateSubsystems(int index) { //TODO: test subsystem functionality
+void Path::updateSubsystems(int index) {
     for (int i = 0; i < this->subsysRecordings[index].size(); i++) {
         this->config.subsysStates[i] = this->subsysRecordings[index][i]; // loop through all subsystem states and update
     }
@@ -76,10 +76,10 @@ void Path::ramseteStep(int index) {
     float beta = this->config.beta; // beta and zeta values fetched
     float zeta = this->config.zeta;
 
-    lemlib::Pose target = this->pathRecordings[index]; // target pose fetched
+    lemlib::Pose target = this->pathRecordings[index]; // target pose fetched, in inches + rad
 
-    float targetX = toMeters(target.x); //in meters
-    float targetY = toMeters(target.y); //in meters
+    float targetX = toMeters(target.x); //x in meters
+    float targetY = toMeters(target.y); //y in meters
     float targetTheta = target.theta;
 
     float linearVelTarget = toMeters(this->velRecordings[index][0]); // target velocities fetched
@@ -89,7 +89,22 @@ void Path::ramseteStep(int index) {
 
     float errorLateral = findLateralError(targetX, targetY); // lateral or crosstrack error calculated in meters
     float errorLongitudinal = findLongitudinalError(targetX, targetY); // longitudinal or front/back error calculated in meters
-    float errorTheta = fmod((targetTheta - pose.theta), M_PI*2); // angular error calculated
+
+    //corrected such that current pose 0, target somewhere else
+    //if targetTheta 350, pose 10, corrected target 340, corrected current 0, true error -20
+    //if targetTheta 10, pose 350, corrected target -340, corrected current 0, true error 20
+    //if targetTheta 100, pose 10, corrected target 90, corrected current 0, true error 90
+    //if targetTheta 300, pose 30, corrected target 270, corrected current 0, true error -90
+
+    float errorTheta = targetTheta - pose.theta; //TODO: check angle
+
+    if (std::abs(errorTheta) > M_PI) {
+        if (errorTheta > 0) {
+            errorTheta -= 2*M_PI;
+        } else {
+            errorTheta += 2*M_PI;
+        }
+    }
 
     float gain = 2 * zeta * std::sqrt(
         std::pow(angularVelTarget, 2) + (beta * std::pow(linearVelTarget, 2))
@@ -132,45 +147,34 @@ void Path::ramseteStep(int index) {
     this->config.leftMotors.move_velocity(leftRPMCommand); // velocity sent to motors
     this->config.rightMotors.move_velocity(rightRPMCommand);
 
-    debugLine.append("current x inches: " + std::to_string(pose.x) + "\n"); //*DEBUG LINES FOR CURRENT XYTHETA
-    debugLine.append("current y inches: " + std::to_string(pose.y) + "\n");
+    debugData["curr x in"] = pose.x;
+    debugData["curr y in"] = pose.y;
+    debugData["curr theta rad"] = pose.theta;
+    debugData["curr theta deg"] = pose.theta / M_PI * 180;
 
-    debugLine.append("current x meters: " + std::to_string(toMeters(pose.x)) + "\n"); //*DEBUG LINES FOR CURRENT XYTHETA
-    debugLine.append("current y meters: " + std::to_string(toMeters(pose.y)) + "\n");
+    debugData["target x in"] = target.x;
+    debugData["target y in"] = target.y;
+    debugData["target theta rad"] = target.theta;
+    debugData["target theta deg"] = target.theta / M_PI * 180;
 
-    debugLine.append("current theta rad: " + std::to_string(pose.theta) + "\n");
+    debugData["error lat in"] = toInches(errorLateral);
+    debugData["error long in"] = toInches(errorLongitudinal);
+    debugData["error theta rad"] = errorTheta;
+    debugData["error theta deg"] = errorTheta / M_PI * 180;
+    
+    debugData["gain"] = gain;
+    debugData["target linear vel in"] = toInches(linearVelTarget);
+    debugData["target angular vel rad"] = angularVelTarget;
+    
+    debugData["cmd linear vel in"] = linearVelCommand;
+    debugData["cmd tangent vel in"] = tangentialVelCommand;
+    debugData["cmd angular vel rad"] = angularVelTarget;
 
-    debugLine.append("target x inches: " + std::to_string(toInches(targetX)) + "\n"); //*DEBUG LINES FOR TARGET XYTHETA
-    debugLine.append("target y inches: " + std::to_string(toInches(targetY)) + "\n");
+    debugData["curr left rpm"] = this->config.leftMotors.get_actual_velocity();
+    debugData["curr right rpm"] = this->config.leftMotors.get_actual_velocity();
 
-    debugLine.append("target x meters: " + std::to_string(targetX) + "\n"); //*DEBUG LINES FOR TARGET XYTHETA
-    debugLine.append("target y meters: " + std::to_string(targetY) + "\n");
-
-    debugLine.append("target theta rad: " + std::to_string(targetTheta) + "\n");
-
-    debugLine.append("error lateral inches: " + std::to_string(toInches(errorLateral)) + "\n"); //*DEBUG LINES FOR ERRORS
-    debugLine.append("error longitudinal inches: " + std::to_string(toInches(errorLongitudinal)) + "\n");
-
-    debugLine.append("error lateral meters: " + std::to_string(errorLateral) + "\n"); //*DEBUG LINES FOR ERRORS
-    debugLine.append("error longitudinal meters: " + std::to_string(errorLongitudinal) + "\n");
-
-    debugLine.append("error theta: " + std::to_string(errorTheta) + "\n");
-
-    debugLine.append("current left rpm: " + std::to_string(this->config.leftMotors.get_actual_velocity()) + "\n"); //*DEBUG LINES FOR CURRENT RPMS
-    debugLine.append("current right rpm: " + std::to_string(this->config.rightMotors.get_actual_velocity()) + "\n");
-
-    debugLine.append("gain: " + std::to_string(gain) + "\n"); //*DEBUG LINES FOR GAIN
-
-    debugLine.append("linear vel target meters: " + std::to_string(linearVelTarget) + "\n"); //*DEBUG LINES FOR VEL TARGETS
-    debugLine.append("linear vel target inches: " + std::to_string(toInches(linearVelTarget)) + "\n"); //*DEBUG LINES FOR VEL TARGETS
-    debugLine.append("angular vel target: " + std::to_string(angularVelTarget) + "\n");
-
-    debugLine.append("linear vel command inches: " + std::to_string(linearVelCommand) + "\n");//*DEBUG LINES FOR VEL COMMANDS
-    debugLine.append("angular vel command: " + std::to_string(angularVelCommand) + "\n");
-    debugLine.append("tangential vel command inches: " + std::to_string(tangentialVelCommand) + "\n");
-
-    debugLine.append("left rpm command: " + std::to_string(leftRPMCommand) + "\n"); //*DEBUG LINE FOR LEFT AND RIGHT RPM
-    debugLine.append("right rpm command: " + std::to_string(rightRPMCommand) + "\n\n\n"); //*LAST DEBUG LINE
+    debugData["cmd left rpm"] = leftRPMCommand;
+    debugData["cmd right rpm"] = rightRPMCommand;
 }
 
 void Path::follow() {
@@ -182,11 +186,9 @@ void Path::follow() {
 
     while (true) {
 
-        debugLine.append("NEW TICK\n\n");
-
         index = findClosestPoint(this->config.chassis.getPose(), index);
 
-        debugLine.append("closest index: " + std::to_string(index) + "\n\n");
+        debugData["index"] = index;
 
         if (index == -1) {
             break;
@@ -195,11 +197,10 @@ void Path::follow() {
         ramseteStep(index);
         updateSubsystems(index);
 
-        debugFile << debugLine;
-        debugLine.clear();
+        debugFile << debugData.dump();
 
         pros::delay(10);
-
+        
     }
 
     return;
